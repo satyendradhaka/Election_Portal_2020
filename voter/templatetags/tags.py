3d2b3e34f460ac990671574e624d57b86d151fc7
Saket Kumar Singh <saketkumar1202@gmail.com>
2020-12-27 23:08:34 +0530

    Modified front end for the vote.html and it's base.

---
+++ voter/templatetags/tags.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+# if you click a vote before reloading time of another vote its showing a glitch 
+# which we think will be resolved when we cahnge the vote to choice fields intead
+# of buttons in templates
+@register.filter
+def to_char(value):
+    return chr(value+64)
