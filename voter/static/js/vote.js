9b6f048f6071db96e00aa805a3e870134c0a2361
Alan T Thomas <alantthomas451@gmail.com>
2020-12-28 11:58:29 +0530

    integrated frontend in svote.html

--- voter/static/js/vote.js
+++ voter/static/js/vote.js
@@ -93,50 +93,15 @@ $(document).ready(function () {
 });
 
 let input_id = null;
+$(document).ready(function () {
 document.onkeyup = function (e) {
-  // if (e.which == 65) {
-  //   input_id = "A";
-  // } else if (e.which == 66) {
-  //   input_id = "B";
-  // } else if (e.which == 67) {
-  //   input_id = "C";
-  // } else if (e.which == 68) {
-  //   input_id = "D";
-  // } else {
-  //   input_id = null;
-  // }
   if(e.which-65>=ids.length){
     input_id = null;
   }
   else{
-    // input_id = String.fromCharCode(e.which);
     input_id = ids[e.which-65];
+    $("#" + input_id).trigger("click");
     console.log(input_id);
   }
-
-  if (input_id) {
-    var cha = String.fromCharCode(65+ids.indexOf(selected_id));
-    $(".voter_list").css("opacity", 1);
-    $(".bg-custom").css("background", "rgb(255, 210, 177)");
-    $(".text-custom").css("color", "#E7690F");
-
-    $("#" + selected_id).removeClass(" bg-gray-200");
-    $("#action_box_container_" + selected_id).removeClass(
-      " bg-black text-white"
-    );
-    $("#action_box_container_" + selected_id).html(
-      `<span id="text_${selected_id}" class="hidden" >Vote for</span><span id="index_${selected_id}">${cha}</span>`
-    );
-    $("#" + input_id)
-      .delay(10)
-      .fadeOut(70)
-      .fadeIn(70)
-      .fadeOut(50)
-      .fadeIn(50);
-    $("#"+input_id).addClass(" bg-gray-200");
-    $("#action_box_container_" + input_id).addClass(" bg-black text-white");
-    $("#action_box_container_" + input_id).html("<span>Voted</span>");
-    selected_id = input_id;
-    selected = true;
-    voteClick(selected_id);
-  }}
+}
+});
