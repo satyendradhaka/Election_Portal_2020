65467cf770b64ac45673c04ea73403c2efd910b6
satyendradhaka <satyendradhaka5@gmail.com>
2021-01-28 01:35:32 +0530

    update backup function

--- results/backup.py
+++ results/backup.py
@@ -6,7 +6,8 @@ filename = 'backup_votes.csv'
 
 def backup_votes(vote1,vote2,vote_time):
     backup_string=[vote1,vote2,vote_time]
-    with open(filename, 'w') as backup:
+    with open(filename, 'a+') as backup:
         backup_writer=csv.writer(backup)
         backup_writer.writerow(backup_string)
+        
     
