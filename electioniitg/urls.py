8977acf24004476d78749e66cffb7153ca49066f
Satyendra Dhaka <satyendradhaka5@gmail.com>
2021-02-13 17:11:26 +0530

    making compatible with deployment url

--- electioniitg/urls.py
+++ electioniitg/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     path('election_portal/oauth2/', include('django_auth_adfs.urls')),
 	path('election_portal/', include('results.urls')),
 	path('election_portal/results/progress',include('celery_progress.urls')),
-	path('', include('stats.urls')),
-	path('', include("preelection.urls")), 
+	path('election/', include('stats.urls')),
+	path('election/', include("preelection.urls")), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # if settings.DEBUG:
