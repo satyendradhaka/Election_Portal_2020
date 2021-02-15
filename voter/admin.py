5ba621d3cf6bdcfba9271db24882775a90d47244
Alan <alantthomas451@gmail.com>
2021-02-15 14:39:54 +0530

    made front end changes in verification

--- voter/admin.py
+++ voter/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Voter,Contestant
-admin.site.register(Voter)
 admin.site.register(Contestant)
 
 # Register your models here.
+class VoterAdmin (admin.ModelAdmin):
+    list_per_page = 1000
+admin.site.register(Voter,VoterAdmin)
